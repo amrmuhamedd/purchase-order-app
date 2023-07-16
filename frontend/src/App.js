32cf44d1d6e87e9b611ff99449c4266e6c41beaa
Amr Mohamed <amrmuhmedd@gmail.com>
2023-07-16 03:20:15 +0300

    add table to display saved data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Form, Input, DatePicker, Button, notification, Upload } from "antd";
+import {
+  Form,
+  Input,
+  DatePicker,
+  Button,
+  notification,
+  Upload,
+  Tabs,
+  Table,
+} from "antd";
 import { InboxOutlined } from "@ant-design/icons";
-
+import moment from "moment";
 const { Dragger } = Upload;
-
+const { TabPane } = Tabs;
 const App = () => {
+  const [purchaseOrders, setPurchaseOrders] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSubmitted, setISubmitted] = useState(false);
+  const [activeTab, setActiveTab] = useState("form");
   const [form] = Form.useForm();
+  const handleTabChange = (key) => {
+    setActiveTab(key);
+  };
+  useEffect(() => {
+    const fetchPurchaseOrders = async () => {
+      setIsLoading(true);
+      const response = await axios.get(
+        "http://localhost:5000/api/purchase-orders"
+      );
+      const data = await response.data;
+      setPurchaseOrders(data);
+      setIsLoading(false);
+    };
 
+    fetchPurchaseOrders();
+  }, [isSubmitted]);
   const handleFormSubmit = async (values) => {
     const { date, vendorName, file } = values;
 
@@ -15,6 +43,7 @@ const App = () => {
     formData.append("date", date.format("YYYY-MM-DD"));
     formData.append("vendorName", vendorName);
     formData.append("file", file[0].originFileObj);
+    console.log(file[0].originFileObj);
     try {
       const response = await axios.post(
         "http://localhost:5000/api/purchase-orders",
@@ -24,6 +53,7 @@ const App = () => {
         message: "Success",
         description: response.data.message,
       });
+      setISubmitted(!isSubmitted);
       form.resetFields();
     } catch (error) {
       notification.error({
@@ -32,6 +62,35 @@ const App = () => {
       });
     }
   };
+
+  const columns = [
+    {
+      title: "Model Number",
+      dataIndex: "modelNumber",
+      key: "modelNumber",
+    },
+    {
+      title: "Unit Price",
+      dataIndex: "unitPrice",
+      key: "unitPrice",
+    },
+    {
+      title: "Quantity",
+      dataIndex: "quantity",
+      key: "quantity",
+    },
+    {
+      title: "Vendor Name",
+      dataIndex: "vendorName",
+      key: "vendorName",
+    },
+    {
+      title: "Date",
+      dataIndex: "date",
+      key: "date",
+      render: (date) => moment(date).format("YYYY-MM-DD"),
+    },
+  ];
 
   return (
     <div
@@ -42,56 +101,70 @@ const App = () => {
         minHeight: "100vh",
       }}
     >
-      <div style={{ width: "600px" }}>
-        <h1 style={{ textAlign: "center" }}>Bulk Purchase Order</h1>
-        <Form form={form} onFinish={handleFormSubmit} layout="vertical">
-          <Form.Item
-            name="date"
-            label="Date"
-            rules={[{ required: true, message: "Please select a date" }]}
-          >
-            <DatePicker />
-          </Form.Item>
-          <Form.Item
-            name="vendorName"
-            label="Vendor Name"
-            rules={[
-              { required: true, message: "Please enter the vendor name" },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            name="file"
-            label="CSV File"
-            valuePropName="fileList"
-            getValueFromEvent={(e) => e.fileList}
-            rules={[{ required: true, message: "Please upload a CSV file" }]}
-          >
-            <Dragger
-              beforeUpload={(e) => {
-                e.preventDefault(); // Prevent default form submission behavior
-                return false; // Prevent file upload
-              }}
-              accept=".csv"
-              maxCount={1}
-            >
-              <p className="ant-upload-drag-icon">
-                <InboxOutlined />
-              </p>
-              <p className="ant-upload-text">
-                Click or drag file to this area to upload
-              </p>
-              <p className="ant-upload-hint">Only CSV files are allowed</p>
-            </Dragger>
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
-      </div>
+      <Tabs activeKey={activeTab} onChange={handleTabChange}>
+        <TabPane tab="Form" key="form">
+          <div style={{ width: "600px" }}>
+            <h1 style={{ textAlign: "center" }}>Bulk Purchase Order</h1>
+            <Form form={form} onFinish={handleFormSubmit} layout="vertical">
+              <Form.Item
+                name="date"
+                label="Date"
+                rules={[{ required: true, message: "Please select a date" }]}
+              >
+                <DatePicker />
+              </Form.Item>
+              <Form.Item
+                name="vendorName"
+                label="Vendor Name"
+                rules={[
+                  { required: true, message: "Please enter the vendor name" },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item
+                name="file"
+                label="CSV File"
+                valuePropName="fileList"
+                getValueFromEvent={(e) => e.fileList}
+                rules={[
+                  { required: true, message: "Please upload a CSV file" },
+                ]}
+              >
+                <Dragger
+                  beforeUpload={(e) => {
+                    e.preventDefault(); // Prevent default form submission behavior
+                    return false; // Prevent file upload
+                  }}
+                  accept=".csv"
+                  maxCount={1}
+                >
+                  <p className="ant-upload-drag-icon">
+                    <InboxOutlined />
+                  </p>
+                  <p className="ant-upload-text">
+                    Click or drag file to this area to upload
+                  </p>
+                  <p className="ant-upload-hint">Only CSV files are allowed</p>
+                </Dragger>
+              </Form.Item>
+              <Form.Item>
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+        </TabPane>
+        <TabPane tab="Table" key="table">
+          <Table
+            dataSource={purchaseOrders}
+            columns={columns}
+            loading={isLoading}
+            rowKey={(record) => record._id}
+          />
+        </TabPane>
+      </Tabs>
     </div>
   );
 };
